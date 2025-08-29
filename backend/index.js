9fa0ddb8dda01472a59a5316bfc933dabeb6244a
ankursasmal <ankursasmal2017@gmail.com>
2025-08-29 23:28:39 +0530

    fix cors from backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://www.loklink.in","*"], 
+  origin: ["https://www.loklink.in", "http://localhost:3000"], 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
