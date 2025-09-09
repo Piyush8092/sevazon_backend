b5d37fd16b9dcc4bfec140b3dca726b41000558a
ankursasmal <ankursasmal2017@gmail.com>
2025-09-09 23:53:19 +0530

    auth add

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,7 +8,8 @@ const authGuard= async(req,res,next)=>{
             return res.status(401).json({message:'Unauthorized'});
         }
         const ExistUser=jwt.verify(token,process.env.SECRET_KEY ||'me333enneffiimsqoqomcngfehdj3idss');
-        if(!ExistUser){
+        // console.log(ExistUser)
+        if(!ExistUser.id){
             return res.status(401).json({message:'Unauthorized'});
         }
         const user=await userModel.findOne({_id:ExistUser.id});
