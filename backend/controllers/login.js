7352ed970d0935813b6d6c3dffc93d5fde6290a6
ankursasmal <ankursasmal2017@gmail.com>
2025-09-11 16:56:21 +0530

    add backend routes for cors

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -36,7 +36,9 @@ const LoginRout = async (req, res) => {
         
         let token = jwt.sign({id: existingUser._id}, process.env.SECRET_KEY ||'me333enneffiimsqoqomcngfehdj3idss', {expiresIn: '1d'});
        
-        res.cookie('jwt', token, {httpOnly: true, maxAge: 1000*60*60*24});
+        res.cookie('jwt', token, {httpOnly: true, maxAge: 365*1000*60*60*24,secure: true,
+      sameSite: "None"});
+
 existingUser.token = token;
         res.json({ message: 'Login successful', status: 200, data: existingUser, success: true, error: false });
     } catch (e) {
--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser());
 app.use(cors({
-  origin: ["https://www.loklink.in","https://loklink-adminportal.vercel.app", "http://localhost:3000"], 
+  origin: ["https://www.loklink.in","https://loklink-adminportal.vercel.app", "http://localhost:3000","https://localhost:3000"], 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
