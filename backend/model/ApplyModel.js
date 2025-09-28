a75f5c3f022a1c0d54b28ace816027901619be45
ankursasmal <ankursasmal2017@gmail.com>
2025-09-28 18:06:33 +0530

    apply model

--- backend/model/ApplyModel.js
+++ backend/model/ApplyModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ApplySchama = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        // required: true
     },
     jobId: {
         type: mongoose.Schema.Types.ObjectId,
