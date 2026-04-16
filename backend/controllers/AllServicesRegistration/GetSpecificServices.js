9ceece19c84ef273b1a81a8cf22e0f3dad4e9a21
Amit Khichar <amitkhichar100@gmail.com>
2026-04-16 20:06:32 +0530

    fix syntax error

--- backend/controllers/AllServicesRegistration/GetSpecificServices.js
+++ backend/controllers/AllServicesRegistration/GetSpecificServices.js
@@ -7,7 +7,13 @@ const GetSpecificServices = async (req, res) => {
 
     const result = await createServiceModel.findById(id);
     if (!result) {
-      res.json({ message: "No data found", status: 400, data: {}, success: false, error: true });
+      return res.json({
+        message: "No data found",
+        status: 400,
+        data: {},
+        success: false,
+        error: true,
+      });
     }
 
     let GetID;
@@ -20,7 +26,7 @@ const GetSpecificServices = async (req, res) => {
     const resultObj = result.toObject();
     resultObj.isBusiness = result.profileType === "Business Profile";
     res.json({
-      message: "Job created successfully",
+      message: "Service fetched successfully",
       status: 200,
       data: { GetID, result: resultObj },
       success: true,
